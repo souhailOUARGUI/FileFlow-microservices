ce9334fde20a78d328f21d7aae00d3e36bfaca2f
SouhailOUARGUI <ouargui550@gmail.com>
2025-12-09 02:36:04 +0100

    Added a common library

---
+++ common-library/src/main/java/org/example/commonlibrary/dto/UserDTO.java
@@ -0,0 +1,13 @@
+package org.example.commonlibrary.dto;
+
+
+import lombok.Data;
+
+@Data
+public class UserDTO {
+    private Long id;
+    private String username;
+    private String email;
+    private String firstName;
+    private String lastName;
+}
---
+++ common-library/src/test/java/org/example/commonlibrary/CommonLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.commonlibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonLibraryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
